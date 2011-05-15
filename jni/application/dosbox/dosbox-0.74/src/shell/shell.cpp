1992ae57248e4667d40febf7d28838f1b3d0cb92
jenvor@gmail.com <jenvor@gmail.com@1a95236e-1df7-11df-b8d3-59fc97e42aa7>
2011-05-15 22:31:57 +0000

    make it compile
    git-svn-id: http://adosbox.googlecode.com/svn/trunk@108 1a95236e-1df7-11df-b8d3-59fc97e42aa7

--- jni/application/dosbox/dosbox-0.74/src/shell/shell.cpp
+++ jni/application/dosbox/dosbox-0.74/src/shell/shell.cpp
@@ -673,7 +673,7 @@ JNIEXPORT void JNICALL Java_org_hystudio_android_dosbox_DOSBoxSettings_nativeDOS
 JNIEXPORT void JNICALL Java_org_hystudio_android_dosbox_DOSBoxSettings_nativeDOSBoxWriteConfig
   (JNIEnv *env, jclass, jstring name) {
     jboolean iscopy;
-    const char *mfile = env->GetStringUTFChars(name, iscopy);
+    const char *mfile = env->GetStringUTFChars(name, &iscopy);
     control->PrintConfig(mfile);
 }
 
