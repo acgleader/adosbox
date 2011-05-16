e0858dbb433f20c2c103193c0b7e4687220d8b3b
jenvor@gmail.com <jenvor@gmail.com@1a95236e-1df7-11df-b8d3-59fc97e42aa7>
2011-05-16 05:53:43 +0000

    Added support for config load/save.
    git-svn-id: http://adosbox.googlecode.com/svn/trunk@111 1a95236e-1df7-11df-b8d3-59fc97e42aa7

--- src/org/hystudio/android/dosbox/DOSBoxSettings.java
+++ src/org/hystudio/android/dosbox/DOSBoxSettings.java
@@ -3,13 +3,16 @@
  */
 package org.hystudio.android.dosbox;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.DialogInterface.OnShowListener;
-import android.net.Uri;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -19,6 +22,7 @@ import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,13 +31,6 @@ import android.widget.Toast;
  * 
  */
 public class DOSBoxSettings {
-
-  // TODO this should be user configurable
-  // Read function CPU_CycleIncrease for details.
-  // If cpu cycle is set to "auto", 5% is increased/decreased each time.
-  // Otherwise, if step is smaller than 100, it is considered as a percentage.
-  // if step is not smaller than 100, it is considered as an absolute vlaue.
-  private static int cpucycle_change = 200;
   private static AlertDialog dialog = null;
 
   protected static void showConfigMainMenu(final MainActivity p) {
@@ -49,6 +46,10 @@ public class DOSBoxSettings {
         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     final ViewGroup dosboxSettingView = (ViewGroup) inflater.inflate(
         R.layout.dosbox_settings, null);
+
+    ScrollView scrollView = (ScrollView) dosboxSettingView
+        .findViewById(R.id.scroll);
+    scrollView.setScrollbarFadingEnabled(false);
 
     final LinearLayout cpuIncLL = (LinearLayout) dosboxSettingView
         .findViewById(R.id.cpucycle_inc_ll);
@@ -200,11 +201,97 @@ public class DOSBoxSettings {
     configSaveLL.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View arg0) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
-        Intent.createChooser(intent, "Choose where to save");
-        p.startActivityForResult(intent, p.SELECT_CONFIG);
+        // TODO: to implement a file chooser
+        // Intent intent = new Intent();
+        // intent.setAction(Intent.ACTION_GET_CONTENT);
+        // intent.setType("*/*");
+        // Intent.createChooser(intent, "Choose where to save");
+        // p.startActivityForResult(intent, p.SELECT_CONFIG);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(p);
+        
+        ScrollView sv = new ScrollView(p);
+        sv.setLayoutParams(new ScrollView.LayoutParams(
+            ScrollView.LayoutParams.WRAP_CONTENT,
+            ScrollView.LayoutParams.WRAP_CONTENT));
+        
+        sv.setScrollbarFadingEnabled(false);
+        LinearLayout ll = new LinearLayout(p);
+        
+        ll.setLayoutParams(new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT));
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        TextView dirTV = new TextView(p);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Dir: " + Settings.getDOSBoxConfDir());
+        sb.append("\nExisting config:\n");
+        boolean isfirst = true;
+        for (File file : listConfigFiles()) {
+          if (!isfirst) {
+            sb.append("\n");
+          }
+          isfirst = false;
+          sb.append("    " + file.getName());
+        }
+        dirTV.setText(sb.toString());
+        dirTV.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        ll.addView(dirTV);
+
+        final EditText filenameET = new EditText(p);
+        filenameET.setSingleLine();
+        filenameET.setWidth(200);
+        ll.addView(filenameET);
+
+        TextView helpView = new TextView(p);
+        helpView.setText("Type name of config file you want to save");
+        helpView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        ll.addView(helpView);
+
+        sv.addView(ll);
+        builder.setView(sv);
+        builder.setTitle("Save config file");
+        AlertDialog dialog = builder.create();
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+            new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface paramDialogInterface,
+                  int paramInt) {
+                String filename = filenameET.getText().toString();
+                final File file = new File(Settings.getDOSBoxConfDir(), filename);
+                if (file.exists()) {
+                  AlertDialog.Builder builder = new AlertDialog.Builder(p);
+                  builder.setTitle("Overwrite exisint file?");
+                  builder.setPositiveButton("Overwrite",
+                      new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                          saveConfigFile(file);
+                        }
+                      });
+                  builder.setNegativeButton("Cancel",
+                      new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                      });
+                  builder.show();
+                } else {
+                  saveConfigFile(file);
+                }
+              }
+            });
+        dialog.show();
+      }
+    });
+
+    final LinearLayout configLoadLL = (LinearLayout) dosboxSettingView
+        .findViewById(R.id.config_load_ll);
+    configLoadLL.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View arg0) {
+        chooseConfigFile(p);
       }
     });
 
@@ -220,6 +307,11 @@ public class DOSBoxSettings {
     dialog.show();
   }
 
+  private static void saveConfigFile(File file) {
+    String absFileName = file.getAbsolutePath();
+    nativeDOSBoxWriteConfig(absFileName);
+  }
+  
   private static void updateView(ViewGroup dosboxSettingView) {
     TextView incTV = (TextView) dosboxSettingView
         .findViewById(R.id.cpucycleshow_inc_tv);
@@ -263,6 +355,73 @@ public class DOSBoxSettings {
     nativeFrameskipDecrease();
   }
 
+  private static String configFileChoosed = null;
+
+  private static void chooseConfigFile(final Activity p) {
+    final List<File> configFiles = listConfigFiles();
+    if (configFiles == null) {
+      Toast.makeText(p, "No config file is found. SD card is busy?", 3000).show();
+      return;
+    }
+    AlertDialog.Builder builder = new AlertDialog.Builder(p);
+    final String[] filenames = new String[configFiles.size()];
+    int idxSelected = -1;
+    for (int i = 0; i < configFiles.size(); ++i) {
+      if (configFiles.get(i).getAbsolutePath().equals(Settings.dosboxConfFileFullPath))
+        idxSelected =i ;
+      filenames[i] = configFiles.get(i).getName();
+    }
+    
+    builder.setTitle("Select config file");
+    builder.setSingleChoiceItems(filenames, idxSelected,
+        new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            configFileChoosed = configFiles.get(which).getAbsolutePath();
+          }
+        });
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        if (configFileChoosed == null) {
+          Toast.makeText(p, "You have not choosen any file", 2000).show();
+          return;
+        }
+        
+        String oldCmdLine = Globals.CommandLine;
+        Globals.CommandLine = "dosbox -conf " + configFileChoosed;
+        Settings.Save(p);
+        Globals.CommandLine = oldCmdLine;
+      }
+    });
+    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+
+      }
+    });
+    builder.show();
+  }
+  
+  private static List<File> listConfigFiles() {
+    String strDestDir = Settings.getDOSBoxConfDir();
+    File destDir = new File(strDestDir);
+    if (!destDir.isDirectory()) {
+      Log.e("aDOSBox", "config dir " + destDir + " does not exist");
+      return null;
+    }
+    File[] files = destDir.listFiles(new FilenameFilter() {
+      @Override
+      public boolean accept(File dir, String filename) {
+        if (filename.endsWith(".conf"))
+          return true;
+        return false;
+      }
+    });
+    List<File> configFiles = Arrays.asList(files);
+    return configFiles;
+  }
+
   static {
     String libs[] = { "application", "sdl_main" };
     try {
@@ -294,6 +453,8 @@ public class DOSBoxSettings {
   private static native void nativeSetCPUCycleDown(int cycledown);
 
   private static native int nativeGetFrameskip();
-  
+
   static native int nativeDOSExit();
+
+  static native int nativeDOSBoxWriteConfig(String absFileName);
 }
