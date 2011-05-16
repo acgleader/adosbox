3bb89eda0ff765426f82dc7065f43c75958e745b
jenvor@gmail.com <jenvor@gmail.com@1a95236e-1df7-11df-b8d3-59fc97e42aa7>
2011-05-16 18:51:37 +0000

    refined clean script. add one option ./clean.sh all
    git-svn-id: http://adosbox.googlecode.com/svn/trunk@112 1a95236e-1df7-11df-b8d3-59fc97e42aa7

--- clean.sh
+++ clean.sh
@@ -4,8 +4,20 @@
 find . -name "*.o"|xargs -I{} rm {}
 find . -name "*.so"|xargs -I{} rm {}
 find . -name "*.a"|xargs -I{} rm {}
-find . -name "Makefile" | xargs -I{} rm {}
-find . -name "config.log" | xargs -I{} rm {}
-find . -name "config.status" | xargs -I{} rm {}
-find . -name "config.h" | xargs -I{} rm {}
+
+if (( $# > 0 )); then
+  howclean=$1
+  case "$howclean" in
+    all)
+      echo "clean all"
+      exit
+      find . -name "Makefile" | xargs -I{} rm {}
+      find . -name "config.log" | xargs -I{} rm {}
+      find . -name "config.status" | xargs -I{} rm {}
+      rm jni/application/dosbox/dosbox-0.74/config.h
+      ;;
+    *)
+      echo "unrecognized parameter \"$howclean\""
+  esac
+fi
 
